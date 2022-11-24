37f2c41b260f6e9029c95ab076cd6af0353ac2e5
techadmine4m <techadmin@exchange4media.com>
2022-11-24 06:11:21 +0000

    all files

--- app/User.php
+++ app/User.php
@@ -14,6 +14,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $table = "users-kubernetes";
+
+    
     protected $fillable = [
         'name', 'email', 'password',
     ];
